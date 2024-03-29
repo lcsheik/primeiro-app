af5dac8ca79bbc06d17ef171a8663a6ebde3b053
lcsheik <165028170+lcsheik@users.noreply.github.com>
2024-03-29 17:38:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppTestSideMenu',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
